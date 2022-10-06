16d690e32e91094297e879e4de7147512147f629
eminn <eminn@DESKTOP-3IALIBH>
2022-10-07 00:26:38 +0400

    Proje dosyası ekle.

---
+++ Food-Machine/Form1.cs
@@ -0,0 +1,111 @@
+using System.Xml.Serialization;
+
+namespace Food_Machine
+{
+    public partial class Form1 : Form
+    {
+        private float total = 0;
+        private float enteredMoney = 0;
+        private List<string> items = new();
+
+        public Form1()
+        {
+            InitializeComponent();
+            label3.Visible = false;
+        }
+
+        private void btn_IceTea_Click(object sender, EventArgs e)
+        {
+            total += float.Parse((sender as Button).Text);
+            Cash.Text = total.ToString();
+            Recept_lstbx.Items.Add((sender as Button).Name);
+        }
+
+        private void birmanat_btn_Click(object sender, EventArgs e)
+        {
+            enteredMoney += float.Parse((sender as Button).Text);
+            MoneyEntered.Text = enteredMoney.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Recept_lstbx.Items.Clear();
+            Cash.Text = "0";
+            total = 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Recept_lstbx.Items.Remove(Recept_lstbx.SelectedItem);
+        }
+
+        private void MoneyEntered_TextChanged(object sender, EventArgs e)
+        {
+            if (MoneyEntered.Text == "0" || MoneyEntered.Text.StartsWith("-"))
+            {
+                MessageBox.Show("Mezelenirsen brat?");
+                MoneyEntered.Text = "";
+            }
+
+            if(MoneyEntered.Text != "")
+                enteredMoney = float.Parse(MoneyEntered.Text);
+
+            if(total > enteredMoney)
+            {
+                label3.Visible = true;
+            }
+            else
+                label3.Visible = false;
+
+        }
+
+        private void x_Click(object sender, EventArgs e)
+        {
+            var xml = new XmlSerializer(typeof(List<string>));
+            using FileStream fs = new(enteredMoney.ToString() + ".xml", FileMode.OpenOrCreate);
+
+            foreach  (var item in Recept_lstbx.Items)
+            {
+                items.Add(item.ToString());
+            }
+            xml.Serialize(fs, items);
+
+            if (total > enteredMoney)
+                MessageBox.Show("Kasib, pulun yoxdu. Hele orda o boyda yazilibe catmir pulun");
+            else
+            {
+                if(enteredMoney - total != 0)
+                    MessageBox.Show($"Buyur brat puluvun qaligi {enteredMoney - total}");
+                else
+                    MessageBox.Show($"Sagol brat cano yiyim");
+            }
+
+            Recept_lstbx.Items.Clear();
+            total = 0;
+            enteredMoney = 0;
+            MoneyEntered.Text = "";
+            Cash.Text = "";
+            items.Clear();
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            var xml = new XmlSerializer(typeof(List<string>));
+            using FileStream fs = new(LoadRecept_txt.Text + ".xml", FileMode.Open);
+            if (fs.CanRead == false)
+                MessageBox.Show("Bibi oglu sehvdiye bu bele sey olar?");
+            else
+            {
+                items = xml.Deserialize(fs) as List<string>;
+
+                foreach (var item in items)
+                {
+                    Recept_lstbx.Items.Add(item);
+                }
+            }
+
+        }
+    }
+}
